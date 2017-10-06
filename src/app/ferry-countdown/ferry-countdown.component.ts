2fb3d0aeaa6bd2b1576cb778bb49f92ad7731ce3
erikwiklander <erik.wiklander@wiklandia.com>
2017-10-06 20:53:36 +0200

    Upgraded dependencies and fixed a few bugs

--- src/app/ferry-countdown/ferry-countdown.component.ts
+++ src/app/ferry-countdown/ferry-countdown.component.ts
@@ -23,15 +23,16 @@ export class FerryCountdownComponent implements OnInit {
   constructor(private ttService: TimetableService) { }
 
   ngOnInit() {
-
+    this.updateValues();
     window.setInterval(() => {
+      this.updateValues();
+    }, 1000);
+  }
+
+  updateValues() {
       const now = new Date();
       this.nextDeparture = this.ttService.nextDeparture(now);
       this.nextDepartureDate = this.nextDeparture.date;
       this.countdown = this.nextDepartureDate.getTime() - now.getTime();
-    }, 1000);
-
-
   }
-
 }
